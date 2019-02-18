377b1fffd5fcf54b3dd776d9acf81136ccb4ff23
alexgracas <alex.pgracas@gmail.com>
2019-02-17 22:29:33 -0300

    * Program.cs: * Startup.cs: * appsettings.json: * site.js: * favicon.ico: * site.css: * site.min.js: * _ViewStart.cshtml: * About.cshtml: * Index.cshtml: * ErrorViewModel.cs: * site.min.css: * Error.cshtml: * Privacy.cshtml: * Contact.cshtml: * _ViewImports.cshtml: * banner3.svg: * banner1.svg: * banner2.svg: * _Layout.cshtml: * appsettings.Development.json: * LICENSE: * .bower.json: * LICENSE.txt: * launchSettings.json: * jquery.js: * .bower.json: * npm.js: * jquery.min.js: * jquery.min.map: * LICENSE.md: * .bower.json: * _CookieConsentPartial.cshtml: * bootstrap.js: * bootstrap.css: * _ValidationScriptsPartial.cshtml: * bootstrap.min.js: * bootstrap.css.map: * bootstrap.min.css: * bootstrap-theme.css: * bootstrap.min.css.map: * jquery.validate.js: * LICENSE.txt: * .bower.json: * bootstrap-theme.min.css: * bootstrap-theme.css.map: * additional-methods.js: * jquery.validate.min.js: * bootstrap-theme.min.css.map: * additional-methods.min.js: * glyphicons-halflings-regular.ttf: * glyphicons-halflings-regular.svg: * glyphicons-halflings-regular.eot: * glyphicons-halflings-regular.woff: * glyphicons-halflings-regular.woff2: * jquery.validate.unobtrusive.js: * jquery.validate.unobtrusive.min.js: Criando o projeto. Adicionando   identidade e autorização.
    * Battle-Board-Games.csproj:
    * HomeController.cs:

---
+++ Battle-Board-Games/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BattleBoardGame.Models;
+
+namespace BattleBoardGame.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Aplicação desenvolvida para o curso de Análise" +
+            	"e Desenvolvimento de Sistemas da Universidade Positivo.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Contatos do Professor.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+       
+
+        private readonly Model.ModelJogosDeGuerra _context;
+        public HomeController(Model.ModelJogosDeGuerra context)
+        {
+            this._context = context;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            bool usuarioAutenticado = true;
+/*                Utils.Utils.ObterUsuarioLogado(
+                    ctx
+                    ) != null;
+*/
+            if (!usuarioAutenticado)
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+
+
+
+        public ActionResult Tabuleiro(int BatalhaId = -1)
+        {
+            ViewBag.Title = "Tabuleiro";
+            var batalha = _context.Batalhas
+                   .Where(b => b.Id == BatalhaId).FirstOrDefault();
+            if (batalha != null)
+                return View(batalha);
+            return View();
+        }
+
+        public ActionResult Login(string usuario, string password, string rememberme, string returnurl)
+        {
+            return View();
+        }
+    }
+}
---
+++ Model-Battle-Board-Games/Exercito.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogosDeGuerraModel
+{
+    [DataContract(IsReference=true)]
+    public class Exercito
+    {
+        public override bool Equals(object obj)
+        {
+            if(obj is Exercito)
+                return ((Exercito)obj).Id == this.Id;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        [InverseProperty("Exercito")]
+        public ICollection<ElementoDoExercito> Elementos { get; set; } =
+            new HashSet<ElementoDoExercito>();
+        [DataMember]
+        public int? BatalhaId { get; set; }
+
+        [ForeignKey("BatalhaId")]
+        public Batalha Batalha { get; set; }
+        [DataMember]
+        public int UsuarioId { get; set; }
+        [DataMember]
+        [ForeignKey("UsuarioId")]
+        public Usuario Usuario { get; set; }
+        [DataMember]
+        public AbstractFactoryExercito.Nacao Nacao { get; set; }
+
+        public ICollection<ElementoDoExercito> ElementosVivos { get
+            {
+                var resultado = from elemento in Elementos
+                                where elemento.Saude > 0
+                                select elemento;
+                return resultado.ToList();
+            }
+        }
+    }
+}
